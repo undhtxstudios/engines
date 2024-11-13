8650cbed85b796fd02ff8570e31691d6113a1dee
Ruchit Patel <ruchittpatell@gmail.com>
2024-11-12 22:24:58 -0800

    shader from source

--- littlejs/tutorials/breakout/game.js
+++ littlejs/tutorials/breakout/game.js
@@ -79,6 +79,8 @@ function gameInit() {
   new Wall(vec2(-0.5, levelSize.y / 2), vec2(1, 100));
   new Wall(vec2(levelSize.x + 0.5, levelSize.y / 2), vec2(1, 100));
   new Wall(vec2(levelSize.x / 2, levelSize.y + 0.5), vec2(100, 1));
+
+  setupPostProcess(); // shader
 }
 
 function gameUpdate() {
@@ -103,6 +105,57 @@ function gameRenderPost() {
     font.drawText("You Win!", cameraPos.add(vec2(0, -5)), 0.2, true);
   else if (!ball)
     font.drawText("Click to Play", cameraPos.add(vec2(0, -5)), 0.2, true);
+}
+
+function setupPostProcess() {
+  const televisionShader = `
+    // Simple TV Shader Code
+    float hash(vec2 p)
+    {
+        p=fract(p*.3197);
+        return fract(1.+sin(51.*p.x+73.*p.y)*13753.3);
+    }
+    float noise(vec2 p)
+    {
+        vec2 i=floor(p),f=fract(p),u=f*f*(3.-2.*f);
+        return mix(mix(hash(i),hash(i+vec2(1,0)),u.x),mix(hash(i+vec2(0,1)),hash(i+1.),u.x),u.y);
+    }
+    void mainImage(out vec4 c, vec2 p)
+    {
+        // put uv in texture pixel space
+        p /= iResolution.xy;
+
+        // apply fuzz as horizontal offset
+        const float fuzz = .0005;
+        const float fuzzScale = 800.;
+        const float fuzzSpeed = 9.;
+        p.x += fuzz*(noise(vec2(p.y*fuzzScale, iTime*fuzzSpeed))*2.-1.);
+
+        // init output color
+        c = texture(iChannel0, p);
+
+        // chromatic aberration
+        const float chromatic = .002;
+        c.r = texture(iChannel0, p - vec2(chromatic,0)).r;
+        c.b = texture(iChannel0, p + vec2(chromatic,0)).b;
+
+        // tv static noise
+        const float staticNoise = .1;
+        c += staticNoise * hash(p + mod(iTime, 1e3));
+
+        // scan lines
+        const float scanlineScale = 1e3;
+        const float scanlineAlpha = .1;
+        c *= 1. + scanlineAlpha*sin(p.y*scanlineScale);
+
+        // black vignette around edges
+        const float vignette = 2.;
+        const float vignettePow = 6.;
+        float dx = 2.*p.x-1., dy = 2.*p.y-1.;
+        c *= 1.-pow((dx*dx + dy*dy)/vignette, vignettePow);
+    }`;
+
+  initPostProcess(televisionShader);
 }
 
 engineInit(gameInit, gameUpdate, gameUpdatePost, gameRender, gameRenderPost);
